109364cce36416a8100bfe15fed997ff47524a6e
ALLEN <aljay3334@gmail.com>
2022-03-05 00:34:56 +0000

    Added product controller

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+
+import { connect } from '../database'
+import { Product } from '../interface/Product';
+
+export async function getProducts(req: Request, res: Response): Promise<Response> {
+    const conn = await connect();
+    const products = await conn.query('SELECT * FROM products');
+    return res.json(products[0]);
+}
+
+export async function createProduct(req: Request, res: Response) {
+    const newProduct: Product = req.body;
+    const conn = await connect();
+    await conn.query('INSERT INTO products SET ?', [newProduct]);
+    return res.json({
+        message: 'Product Created'
+    });
+}
+
+export async function getSingleProduct(req: Request, res: Response): Promise<Response> {
+    const id = req.params.id;
+    const conn = await connect();
+    const product = await conn.query('SELECT * FROM products WHERE id = ?', [id]);
+
+    return res.json(product[0]);
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+    const id = req.params.id;
+    const conn = await connect();
+    await conn.query('DELETE FROM products WHERE id = ?', [id]);
+    return res.json({
+        message: 'Product deleted'
+    });
+}
+
+export async function updateProduct(req: Request, res: Response) {
+    const id = req.params.id;
+    const updatedProduct: Product = req.body;
+    const conn = await connect();
+    await conn.query('UPDATE products SET ? WHERE id = ?', [updatedProduct, id]);
+    return res.json({
+        message: 'Product updated'
+    });
+}
